2a7f1e5d9ef91fb5966b3df04cf40359e6cfde28
IEIrodov <nikhilindia30111999@gmail.com>
2021-02-25 13:14:06 +0530

    Refactoring Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+
+//Components and Pages
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Ignite</h1>
+      <Home />
     </div>
   );
 }
